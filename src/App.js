5feecc1e9fc3de4fd9e721ca5a8615bae9ab3b39
root <colman.jin94@gmail.com>
2018-04-09 21:40:02 -0400

    Using kirupa SPA react_router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, NavLink, HashRouter} from "react-router-dom";
+
+import Home from "./Home";
+import About from "./About";
+//import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <HashRouter>
+        <div>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+          </ul>
+          <h1>Simple SPA</h1>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+          </div>
+        </div>
+      </HashRouter>
+
+      /*<div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,7 +30,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </div>*/
     );
   }
 }
